c6fc6f459059df6094478608915284d56f9eb28e
Jay Uhl <juhl@40t-juhl-110100.pvrhsd.org>
2019-02-01 09:22:48 -0500

    Add new page... 2

--- deploy.py
+++ deploy.py
@@ -3,5 +3,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
    return render_template("index.html")
+
+@app.route("/page2")
+def page_2():
+   return render_template("page2.html")
+
 if __name__ == "__main__":
    app.run(debug=True)
