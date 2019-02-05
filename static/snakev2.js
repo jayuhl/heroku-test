185fbbd64c62bbe3367986d9562e437b748c0e75
Jay Uhl <juhl@pascack.org>
2019-02-04 23:20:01 -0500

    new snake attempt 2

--- static/snakev2.js
+++ static/snakev2.js
@@ -3,12 +3,69 @@ function startGame() {
   }
   
   var myGameArea = {
-    canvas : document.createElement("canvas"),
+    canv : document.createElement("canvas"),
     start : function() {
-      this.canvas.width = 480;
-      this.canvas.height = 270;
-      this.context = this.canvas.getContext("2d");
-      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      console.log('Canvas created.')
+      this.canv.width = 600;
+      this.canv.height = 500;
+      this.ctx = this.canvas.getContext("2d");
+      document.body.insertBefore(this.canv, document.body.childNodes[0]);
+      document.addEventListener("keydown",keyPush);
+      setInterval(game,1000/15);
     }
   }
+
+px=py=10;
+gs=tc=20;
+ax=ay=15;
+xv=yv=0;
+trail=[];
+tail = 5;
+
+function game() {
+	px+=xv;
+	py+=yv;
+
+	if(px < 0)    {px = canv.width/tc-1;} //allow snake to pass-through the screen
+	if(px > canv.width/tc-1) {px = 0;}
+	if(py < 0)    {py = canv.height/tc-1;}
+	if(py > canv.height/tc-1) {py = 0;}
+
+	ctx.fillStyle="black";
+	ctx.fillRect(0,0,canv.width,canv.height);
+
+	ctx.fillStyle="lime";
+	for(var i=0;i<trail.length;i++) {
+		ctx.fillRect(trail[i].x*gs,trail[i].y*gs,gs-2,gs-2);
+		if(trail[i].x==px && trail[i].y==py) {
+			tail = 5;
+		}
+	}
+	trail.push({x:px,y:py});
+	while(trail.length > tail) {
+		trail.shift();
+	}
+
+	if(ax==px && ay==py) {
+		tail++;
+		ax=Math.floor(Math.random()*canv.width/tc);
+		ay=Math.floor(Math.random()*canv.height/tc);
+	}
+	ctx.fillStyle="red";
+	ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
+}
+function keyPush(evt) {
+	switch(evt.keyCode) {
+		case 37:
+			xv=-1;yv=0;
+			break;
+		case 38:
+			xv=0;yv=-1;
+			break;
+		case 39:
+			xv=1;yv=0;
+			break;
+		case 40:
+			xv=0;yv=1;
+			break;
+	}
+}
